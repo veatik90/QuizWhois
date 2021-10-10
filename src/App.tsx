7b7c3d5e9143b305e86b44fe6b7272b7d9287454
svyatoslav platonov <svyatoslav.platonov93@gmail.com>
2021-10-10 19:12:10 +0300

    Revert "Add files via upload"
    This reverts commit efae970675bc79cd57d9e130659f876b8beb5766.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { NoMatch } from "./pages/NoMatch";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <p>home</p>
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
