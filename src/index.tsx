ce81db5ee674a2688e095271d296bf8b2a432bb3
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2021-10-10 16:15:17 +0300

    feat: add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {ThemeProvider, createTheme} from '@mui/material'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#61dafb'
+    },
+    secondary: {
+      main: '#da61bf'
+    }
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>   
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
