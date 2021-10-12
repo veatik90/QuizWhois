4e4e3a8578c2a19f400ea11117516110ace28058
svyatoslav platonov <74401377+varenechko@users.noreply.github.com>
2021-10-12 09:18:11 +0300

    feat: added 404 page
    there wasn't few dependencies, i added them

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,1 +1,29 @@
-export const NotFound = () => <p>404 - page not found</p>;
+import { Box, Button, Stack, ThemeProvider, Typography } from '@mui/material';
+import { theme } from '../../themes';
+
+
+export const NotFound = () => 
+    <Box
+    sx={{
+    width: '100%',
+    minHeight: '100vh',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center'
+    }}
+    >
+      <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      >
+        <ThemeProvider theme={theme}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQAw3md2--EMzzKYG7AyImi9BeJvaJLO4riQ&usqp=CAU" alt="telescope" style={{borderRadius:'50%'}}/>
+            <Typography variant="subtitle1" align='center'>
+                404 | Page not found
+            </Typography>
+            <Button href='/' variant='outlined' color='primary' size='large'>home</Button>
+        </ThemeProvider>
+    </Stack>
+    </Box>
--- src/themes/index.tsx
+++ src/themes/index.tsx
@@ -1,5 +1,5 @@
 import {createTheme} from '@mui/material'
-import './index.css';
+import '../index.css';
 
 export const theme = createTheme({
   palette: {
