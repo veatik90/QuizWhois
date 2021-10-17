fb9236d260bf6468d425adc0addb1897c6bd910f
Владислав Сусып <clear.live@bk.ru>
2021-10-17 21:16:18 +0300

    fix: add type script types

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-// @ts-nocheck
 import { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { HTTP_METHOD, useAxios } from './hooks/useAxios';
+import { HttpMethod, useAxios } from './hooks/useAxios';
 import { NotFound } from './pages/NotFound';
 
+function urlExample() {
+  const apiKey = 'AIzaSyBKTdCye6Aaxf0iXTw-jTCtUww46_cB-xI';
+  return {
+    postError: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+    postSuccess: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
+  };
+}
+const body = { email: 'vasdsxxad@gmail.com', password: '234234ddfdsfedac', returnSecureToken: true };
+
 const App: FC = () => {
-  const { data, error, isLoading } = useAxios(
-    HTTP_METHOD.POST,
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBKTdCye6Aaxf0iXTw-jTCtUww46_cB-xI',
-    { email: 'vasdsxxad@gmail.com', password: '234234ddfdsfedac', returnSecureToken: true },
-  );
+  const { data, error, isLoading } = useAxios(HttpMethod.POST, urlExample().postSuccess, body);
+  // eslint-disable-next-line no-console
+  console.log(data, error, isLoading);
 
   return (
     <Router>
--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,21 +1,27 @@
-// @ts-nocheck
 import { useEffect, useState } from 'react';
+import { AxiosResponse, AxiosError } from 'axios';
 import { axiosInstance } from '../configs/axios';
 
-export enum HTTP_METHOD {
+export enum HttpMethod {
   GET = 'get',
   POST = 'post',
 }
 
-export function useAxios(requestType: HTTP_METHOD, url: string, body?: any) {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+export function useAxios(requestType: HttpMethod, url: string, body?: Record<string, unknown>) {
+  const [data, setData] = useState<AxiosResponse | null>(null);
+  const [error, setError] = useState<AxiosResponse | undefined | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     axiosInstance[requestType](url, body)
-      .then((data) => setData(data))
-      .catch((error) => setError(error))
+      .then((response: AxiosResponse) => {
+        setData(response);
+        setError(null);
+      })
+      .catch((response: AxiosError) => {
+        setError(response.response);
+        setData(null);
+      })
       .finally(() => setIsLoading(false));
   }, []);
 
