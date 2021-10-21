44a37e8732cb087b3bde3c4aec140ba00c153064
varenechko <svyatoslav.platonov93@gmail.com>
2021-10-21 15:23:46 +0300

    commit2

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { NotFound } from './pages/NotFound';
+
+const App: FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <p>home</p>
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'react';
+
+export const NotFound: FC = () => <p>404 - page not found</p>;
