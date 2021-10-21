4fd65a312169ee018d9502a855729e145d50b3ae
Ilya Kitaev <ilya.kitayev@dataart.com>
2021-10-21 22:42:30 +0300

    fix: eslint problem with arrow func that returns arrow func

--- src/hooks/axios/utils/sendRequest.ts
+++ src/hooks/axios/utils/sendRequest.ts
@@ -1,23 +1,25 @@
-/* eslint-disable */
+/* eslint-disable func-names */
 import { AxiosError, AxiosResponse } from 'axios';
+import { Dispatch } from 'react';
 import { axiosInstance } from '../../../configs/axios';
-import { Dispatch } from "react";
 
 /** onSuccess, onError - обработчики. */
-export const sendRequest = (
+export function sendRequest(
   setOnSuccess: Dispatch<AxiosResponse | null>,
   setOnError: Dispatch<AxiosResponse | undefined | null>,
   setIsLoading: Dispatch<boolean>,
-) =>
-  (requestType: string, url: string, body?: Record<string, unknown>) => {
-  axiosInstance.post(url, body)
-    .then((response: AxiosResponse) => {
-      setOnSuccess(response);
-      setOnError(null);
-    })
-    .catch((response: AxiosError) => {
-      setOnError(response.response);
-      setOnSuccess(null);
-    })
-    .finally(() => setIsLoading(false));
-};
+) {
+  return function (requestType: string, url: string, body?: Record<string, unknown>) {
+    axiosInstance
+      .post(url, body)
+      .then((response: AxiosResponse) => {
+        setOnSuccess(response);
+        setOnError(null);
+      })
+      .catch((response: AxiosError) => {
+        setOnError(response.response);
+        setOnSuccess(null);
+      })
+      .finally(() => setIsLoading(false));
+  };
+}
