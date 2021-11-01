68a4cd9ea72aea38ff581520bc7b8711ee2fd59e
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2021-11-01 18:06:14 +0300

    feat: add theme colors

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,12 +1,48 @@
 import { createTheme } from '@mui/material';
 
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: React.CSSProperties['color'];
+    };
+  }
+
+  interface Palette {
+    neutral: Palette['primary'];
+  }
+  interface PaletteOptions {
+    neutral: PaletteOptions['primary'];
+  }
+
+  interface PaletteColor {
+    darker?: string;
+  }
+  interface SimplePaletteColorOptions {
+    darker?: string;
+  }
+  interface ThemeOptions {
+    status: {
+      danger: React.CSSProperties['color'];
+    };
+  }
+}
 export const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
   palette: {
     primary: {
-      main: '#80619E',
+      main: '#116466',
+      contrastText: '#FFCB9A',
+      darker: '#2c3531',
     },
     secondary: {
-      main: '#439400',
+      main: '#FFCB9A',
+      contrastText: '#D1E8E2',
+    },
+    neutral: {
+      main: '#D9B08C',
+      contrastText: '#D1E8E2',
     },
   },
 });
