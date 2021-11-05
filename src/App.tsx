a0dce478cf901c0894e0c926c1a09e1bd2c5c069
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2021-11-05 17:20:12 +0300

    feat: add redirect to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { Header } from './components/Header';
 import { NotFound } from './pages/NotFound';
 import { Auth } from './pages/Auth';
+import { Dashboard } from './pages/Dashboard';
 
 const App: FC = () => {
   return (
     <Router>
       <Header />
       <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
         <Route exact path="/">
-          <p>home</p>
+          <Redirect to="/dashboard" />
         </Route>
         <Route path="/auth">
           <Auth />
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,6 @@
+import { Box } from '@mui/material';
+import { FC } from 'react';
+
+export const Dashboard: FC = () => {
+  return <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>kkkk</Box>;
+};
