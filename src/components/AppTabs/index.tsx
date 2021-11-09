56a9ff5a7b28a409f9c99626f04b4db759cf5be1
Ilya Kitaev <ilya.kitayev@dataart.com>
2021-11-09 17:54:20 +0300

    fix: direct tab access

--- src/components/AppTabs/index.tsx
+++ src/components/AppTabs/index.tsx
@@ -1,19 +1,14 @@
 import { Tab, Tabs } from '@mui/material';
 import { Box } from '@mui/system';
-import { FC, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { FC } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { appTabsConfig } from '../../configs/appTabs';
-import { Routes } from '../../configs/routes';
 
 export const AppTabs: FC = () => {
-  const [activeTab, setActiveTab] = useState(Routes.CATALOG);
-
-  const handleChange = (event: React.SyntheticEvent, newTab: Routes) => {
-    setActiveTab(newTab);
-  };
+  const { pathname } = useLocation();
   return (
     <Box>
-      <Tabs value={activeTab} onChange={handleChange} centered>
+      <Tabs value={pathname} centered>
         {Object.values(appTabsConfig).map((tab) => (
           <Tab key={tab.order} label={tab.label} value={tab.url} component={Link} to={tab.url} />
         ))}
