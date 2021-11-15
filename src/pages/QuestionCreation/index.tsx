4ac3e87fd0f3ba9c01e45659f2c9f0720202bf9a
varenechko <svyatoslav.platonov93@gmail.com>
2021-11-15 17:10:44 +0300

    feat: add list of questions

--- src/pages/QuestionCreation/index.tsx
+++ src/pages/QuestionCreation/index.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable no-console */
 import { Button, Grid, Stack, TextField } from '@mui/material';
 import { FC, useState } from 'react';
 import { ISynonym, IQuestion } from './interfaces';
+import { BoxWithBorderStyled, GridStyled, QuestionSpanStyled, SavePackButtonStyled } from './styles';
 // import { AnswerFieldStyled, AnswerStyled } from './styles';
 
 export const QuestionCreation: FC = () => {
   const [questions, setQuestions] = useState<IQuestion[]>([]);
+  const [id, setId] = useState(0);
   const [question, setQuestion] = useState('');
   const [answer, setAnswer] = useState('');
   const [answerSynonyms, setAnswerSynonyms] = useState<ISynonym[]>([{ id: 0, synonym: '' }]);
@@ -17,14 +20,14 @@ export const QuestionCreation: FC = () => {
     setAnswer(event.target.value);
   };
 
-  const handleChangeSynonym = (id: number, event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChangeSynonym = (elemId: number, event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const tmpAnswerSynonyms = [...answerSynonyms];
     const synonym = event.target.value;
-    tmpAnswerSynonyms[id].synonym = synonym;
+    tmpAnswerSynonyms[elemId].synonym = synonym;
     setAnswerSynonyms(tmpAnswerSynonyms);
   };
 
-  const handleEnterDown = (id: number, event: React.KeyboardEvent<HTMLInputElement | HTMLDivElement>) => {
+  const handleEnterDown = (elemId: number, event: React.KeyboardEvent<HTMLInputElement | HTMLDivElement>) => {
     if (event.key === 'Enter') {
       const tmpAnswerSynonyms = answerSynonyms.filter((elem) => elem.synonym.length > 0);
       if (tmpAnswerSynonyms.length === answerSynonyms.length) {
@@ -35,23 +38,54 @@ export const QuestionCreation: FC = () => {
     }
   };
 
-  const handleQuestionSave = () => {
+  const handleQuestionSave = (elemId: number) => {
     let tmpQuestions: IQuestion[] = [];
     if (questions) tmpQuestions = [...questions];
     if (question !== '' && answer !== '') {
       setAnswerSynonyms(answerSynonyms.filter((elem) => elem.synonym.length > 0)); // не успевает убрать пустые синонимы
-      tmpQuestions.push({ id: tmpQuestions.length, question, answer, answerSynonyms });
+      if (questions.filter((elem) => elem.id === elemId).length > 0) {
+        tmpQuestions.map((elem, index) => {
+          if (elem.id === elemId) {
+            tmpQuestions[index] = { id, question, answer, answerSynonyms };
+          }
+          return elem;
+        });
+      } else tmpQuestions.push({ id: elemId, question, answer, answerSynonyms });
     }
+    console.log(tmpQuestions);
     setQuestions(tmpQuestions);
     setAnswerSynonyms([{ id: 0, synonym: '' }]);
     setAnswer('');
     setQuestion('');
-    // console.log(questions);
+    setId(tmpQuestions.length);
+  };
+
+  const handleQuestionDelete = (elemId: number) => {
+    const tmpQuestions = questions.filter((elem) => elem.id !== elemId);
+    setQuestions(tmpQuestions);
+    setQuestions(tmpQuestions);
+    setAnswerSynonyms([{ id: 0, synonym: '' }]);
+    setAnswer('');
+    setQuestion('');
+    setId(elemId);
+  };
+
+  const handleQuestionQlick = (elemId: number) => {
+    const [tmpQuestion] = questions.filter((elem) => elem.id === elemId);
+    console.log(tmpQuestion, elemId, questions);
+    setId(tmpQuestion.id);
+    setQuestion(tmpQuestion.question);
+    setAnswer(tmpQuestion.answer);
+    setAnswerSynonyms(tmpQuestion.answerSynonyms);
+  };
+
+  const handleSavePack = () => {
+    console.log('save pack');
   };
 
   return (
-    <Grid container spacing={2} mt={0} px={6} justifyContent="space-between">
-      <Grid item xs={5}>
+    <GridStyled className="QuestionCreationPageBlock" container spacing={2}>
+      <Grid item xs={4}>
         <Stack spacing={2}>
           <TextField label="Question" value={question} multiline rows={4} onChange={handleChangeQuestion} />
           {/* <AnswerFieldStyled>
@@ -71,12 +105,30 @@ export const QuestionCreation: FC = () => {
               onKeyDown={(e) => handleEnterDown(elem.id, e)}
             />
           ))}
-          <Button onClick={handleQuestionSave}>Save question</Button>
+          <Stack direction="row" justifyContent="space-between">
+            <Button variant="outlined" onClick={() => handleQuestionDelete(id)}>
+              Delete question
+            </Button>
+            <Button variant="contained" onClick={() => handleQuestionSave(id)}>
+              Save question
+            </Button>
+          </Stack>
         </Stack>
       </Grid>
-      <Grid item xs={2}>
-        <Button>button</Button>
+      <Grid item xs={4} ml={10}>
+        <BoxWithBorderStyled className="QuestionsBox">
+          <Stack position="static">
+            {questions.map((elem, index) => (
+              <QuestionSpanStyled key={elem.id} onClick={() => handleQuestionQlick(elem.id)}>
+                <p className="questionP">{`${index + 1}) ${elem.question}`}</p>
+              </QuestionSpanStyled>
+            ))}
+          </Stack>
+          <SavePackButtonStyled variant="contained" fullWidth onClick={handleSavePack}>
+            save pack
+          </SavePackButtonStyled>
+        </BoxWithBorderStyled>
       </Grid>
-    </Grid>
+    </GridStyled>
   );
 };
--- src/pages/QuestionCreation/styles.ts
+++ src/pages/QuestionCreation/styles.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { Box, Button, Grid } from '@mui/material';
+import { HEADER_HEIGHT } from '../../shared/constants';
+import { theme } from '../../theme';
 
 export const AnswerFieldStyled = styled.div`
   box-sizing: content-box;
@@ -14,5 +17,49 @@ export const AnswerBlockStyled = styled.span`
 
 export const AnswerStyled = styled.p`
   margin: 0;
-  /* background-color: green; */
+`;
+
+export const GridStyled = styled(Grid)`
+  &.QuestionCreationPageBlock {
+    height: calc(100vh - ${HEADER_HEIGHT}px);
+    justify-content: center;
+    padding-top: 50px;
+    position: relative;
+    margin-top: 0px;
+  }
+`;
+
+export const BoxWithBorderStyled = styled(Box)`
+  &.QuestionsBox {
+    position: relative;
+    border: 1px solid lightgrey;
+    border-radius: 3px;
+    height: 70vh;
+    width: 100%;
+    padding: 14px 16px;
+  }
+`;
+
+export const SavePackButtonStyled = styled(Button)`
+  position: absolute;
+  bottom: 0px;
+  height: 70px;
+  align-items: center;
+  z-index: 1;
+`;
+
+export const QuestionSpanStyled = styled.span`
+  margin-bottom: 10px;
+  & p {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-decoration: none;
+    margin: 0;
+  }
+  &:hover {
+    cursor: pointer;
+    background-color: ${theme.palette.primary.main};
+    color: white;
+  }
 `;
