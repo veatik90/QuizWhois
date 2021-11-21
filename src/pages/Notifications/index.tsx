e8b2724e2fc20bc93b7f6ba7aa7db4bd73fdf0e4
Roza Zhaisanova <57352835+RozaZhaisanova@users.noreply.github.com>
2021-11-21 14:51:18 +0300

    fix: empty state for notiifications

--- src/pages/Notifications/index.tsx
+++ src/pages/Notifications/index.tsx
@@ -10,29 +10,34 @@ export const Notifications: FC = () => {
   const filteredPacks = packs.filter((p) => p.isDraft === true);
   // eslint-disable-next-line no-console
   console.log(setPacks);
+  if (filteredPacks.length === 0) {
+    return (
+      <Container component="main" maxWidth="md">
+        <Typography variant="h4" align="center" color="primary" gutterBottom sx={{ marginTop: 5 }}>
+          Список уведомлений пуст
+        </Typography>
+      </Container>
+    );
+  }
   return (
     <Container component="main" maxWidth="md">
       <Typography variant="h4" align="center" color="primary" gutterBottom sx={{ marginTop: 3 }}>
         Уведомления
       </Typography>
       <Stack mt={2} direction="column" justifyContent="flex-start" alignItems="stretch" spacing={2}>
-        {filteredPacks ? (
-          filteredPacks.map((p) => (
-            <NavLinkStyled key={p.id} to={Routes.MY_CREATED_GAMES}>
-              <Paper elevation={4}>
-                <Alert severity="info">
-                  <Typography>
-                    &nbsp;&nbsp; Не завершено создание пакета&nbsp;&quot;
-                    {p.name}
-                    &quot;
-                  </Typography>
-                </Alert>
-              </Paper>
-            </NavLinkStyled>
-          ))
-        ) : (
-          <>Список уведомлений пуст</>
-        )}
+        {filteredPacks.map((p) => (
+          <NavLinkStyled key={p.id} to={Routes.MY_CREATED_GAMES}>
+            <Paper elevation={4}>
+              <Alert severity="info">
+                <Typography>
+                  &nbsp;&nbsp; Не завершено создание пакета&nbsp;&quot;
+                  {p.name}
+                  &quot;
+                </Typography>
+              </Alert>
+            </Paper>
+          </NavLinkStyled>
+        ))}
       </Stack>
     </Container>
   );
